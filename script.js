cc03f13b9ec480fdba371b32fbf651266c5e3509
danielld75 <danielld@poczta.fm>
2017-11-28 18:59:32 +0100

    Add scriptjs with work

---
+++ script.js
@@ -0,0 +1,15 @@
+function getTriangleArea(a, h) {
+  if (a > 0 && h > 0) {
+    return a * h / 2;
+  } else {
+    return "Nieprawidłowe dane";
+  }
+}
+
+console.log(getTriangleArea(10, 6));
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(0, 15);
+var triangle3Area = getTriangleArea(10, -15);
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
